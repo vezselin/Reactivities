c4cde9a8f9eb639237c6df038f893e42c6f60504
Veselin Popov <veselinpopov1999@gmail.com>
2024-06-01 22:29:47 +0200

    add delete activity

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -39,4 +39,12 @@ public class ActivitiesController : BaseApiController
 
         return Ok();
     }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteActivity(Guid id)
+    {
+        await Mediator.Send(new Delete.Command { Id = id });
+
+        return Ok();
+    }
 }
