ee886fdd5958ddfcbd639ca3c5130f8846b6d690
Veselin Popov <veselinpopov1999@gmail.com>
2025-01-09 00:00:45 +0100

    added login funtionallity

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,42 @@
+using API.UserDTO;
+using Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+
+public class AccountController : ControllerBase
+{
+    private readonly UserManager<AppUser> _userManager;
+
+    public AccountController(UserManager<AppUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+    {
+        var user = await _userManager.FindByEmailAsync(loginDto.Email);
+
+        if (user == null) return Unauthorized();
+
+        var result = await _userManager.CheckPasswordAsync(user, loginDto.Password);
+
+        if (result)
+        {
+            return new UserDto
+            {
+                DisplayName = user.DisplayName,
+                Image = null,
+                Username = user.UserName,
+                Token = "This is a real token"
+            };
+        } 
+
+        return Unauthorized();
+    }
+}
